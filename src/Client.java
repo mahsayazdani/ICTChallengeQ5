aef5461439192d65778733f427b684e083b20e43
Mahsa Yazdani <47636526+mahsayazdani@users.noreply.github.com>
2019-03-07 04:24:42 -0800

    added client

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,28 @@
+
 public class Client {
+   private int entryTime;
+   private int endWaitTime;
+   private int remainWork;
+
+    public Client(int entryTime, int endWaitTime, int remainWork) {
+        this.entryTime = entryTime;
+        this.endWaitTime = endWaitTime;
+        this.remainWork = remainWork;
+    }
+
+    public int getEntryTime() {
+        return entryTime;
+    }
+
+    public int getEndWaitTime() {
+        return endWaitTime;
+    }
+
+    public int getRemainWork() {
+        return remainWork;
+    }
+
+    public void decreaseRemainWork(){
+        remainWork--;
+    }
 }
