94d81a51e6aa38e6de12f5774da758131bcc8402
Morteza Taghaddomi Masoumi <mortezataqaddomi99@gmail.com>
2019-03-07 18:11:48 +0330

    merged

--- src/Main.java
+++ src/Main.java
@@ -9,27 +9,20 @@ public class Main {
         bankCapacity = scan.nextInt();
         n = scan.nextInt();
 
-        Bank bank = new Bank(1 , bankCapacity);
+        Bank bank = new Bank(1, bankCapacity);
         List<Client> clients = new ArrayList<>();
 
-        for(int i = 1, enterTime = 0; i <= n; ++i) {
-<<<<<<< HEAD
-            int servicTime = scan.nextInt();
-            int boredThreshold = scan.nextInt();
-            int entryDelay = scan.nextInt();
-            enterTime += entryDelay;
-            clients.add(new Client(i, servicTime, boredThreshold, enterTime));
-=======
+        for (int i = 1, enterTime = 0; i <= n; ++i) {
+
             int serviceTime = scan.nextInt();
             int boredThreshold = scan.nextInt();
             int entryDelay = scan.nextInt();
             enterTime += entryDelay;
             clients.add(new Client(i, serviceTime, boredThreshold, enterTime));
->>>>>>> 4e2e1858a6a77b442964e3e3f37443f2437b38d4
         }
 
         int time = 0;
-        for(Client client : clients) {
+        for (Client client : clients) {
             try {
                 bank.enter(client);
             } catch (FullCapacityException e) {
@@ -39,5 +32,6 @@ public class Main {
 
     }
 
-    public static void done(Client client, String reason){}
+    public static void done(Client client, String reason) {
+    }
 }
