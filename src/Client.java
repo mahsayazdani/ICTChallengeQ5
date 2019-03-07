5d577ff2bc021927064dd0a621c85468af011cd6
Mahsa Yazdani <47636526+mahsayazdani@users.noreply.github.com>
2019-03-07 04:35:57 -0800

    added id

--- src/Client.java
+++ src/Client.java
@@ -1,10 +1,13 @@
 
 public class Client {
-   private int entryTime;
-   private int endWaitTime;
-   private int remainWork;
 
-    public Client(int entryTime, int endWaitTime, int remainWork) {
+    private int id;
+    private int entryTime;
+    private int endWaitTime;
+    private int remainWork;
+
+    public Client(int id, int entryTime, int endWaitTime, int remainWork) {
+        this.id = id;
         this.entryTime = entryTime;
         this.endWaitTime = endWaitTime;
         this.remainWork = remainWork;
@@ -20,6 +23,10 @@ public class Client {
 
     public int getRemainWork() {
         return remainWork;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public void decreaseRemainWork(){
