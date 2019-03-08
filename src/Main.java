23fbce8ae6a395849a52e8ab6624360410e9616c
Mahsa Yazdani <47636526+mahsayazdani@users.noreply.github.com>
2019-03-08 13:39:17 -0800

    last update of challenge day

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+
 import java.util.List;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -9,29 +11,33 @@ public class Main {
         bankCapacity = scan.nextInt();
         n = scan.nextInt();
 
+
         Bank bank = new Bank(1, bankCapacity);
-        List<Client> clients = new ArrayList<>();
+        ArrayList<Client> clients = new ArrayList<Client>();
 
-        for (int i = 1, enterTime = 0; i <= n; ++i) {
-
-            int serviceTime = scan.nextInt();
+        for (int i = 0, enterTime = 0; i <= n; ++i) {
+            int servicTime = scan.nextInt();
             int boredThreshold = scan.nextInt();
             int entryDelay = scan.nextInt();
             enterTime += entryDelay;
-            clients.add(new Client(i, serviceTime, boredThreshold, enterTime));
+            clients.add(new Client(i+1, servicTime, boredThreshold, enterTime));
         }
 
-        int time = 0;
         for (Client client : clients) {
             try {
+                while (bank.getCurrentTime() < client.getEntryTime()) {
+                    bank.timeLapse();
+                }
                 bank.enter(client);
             } catch (FullCapacityException e) {
-                e.printStackTrace();
+                System.out.println("Banned");
             }
         }
-
+        bank.timeLapse();
     }
 
-    public static void done(Client client, String reason) {
+
+    public static void done(Client client, String reason){
+        System.out.println(client.getId() + ", " + reason);
     }
 }
